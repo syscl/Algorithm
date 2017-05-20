4eda4140c869c923ab3d7dfca0a7e0ff96de398c
syscl <maxblast@163.com>
2017-05-20 18:22:29 -0500

    Optimize style

--- Oracle/scrabbler.c
+++ Oracle/scrabbler.c
@@ -33,6 +33,7 @@ int main(int argc, char **argv)
                     pattern = *(argv+i+1);
                     letterIsMatch = &prefixIsMatch;
                     break;
+                    
                 case SUFFIX:
                     letterIsMatch = &suffixIsMatch;
                     pattern = *(argv+i+1);
